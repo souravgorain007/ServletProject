a2ed20bbe73160096b1ea4d668f4932cbf1ff591
souravgorain007 <souravgorain004@gmail.com>
2021-01-05 12:32:05 +0530

    MultiPurpose Internet Mail Extension servlet program.

---
+++ MIMEApp/src/com/sa/HtmlServlet.java
@@ -0,0 +1,28 @@
+package com.sa;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HtmlServlet extends HttpServlet {
+	public void service(HttpServletRequest req ,HttpServletResponse res) throws ServletException,IOException{
+		
+		//set Content Type
+		res.setContentType("text/html");
+		//get PrintWriter object
+		PrintWriter pw=res.getWriter();
+		//Writer the logic for the HTML format
+		pw.println("<table border='1'>");
+		pw.println("<tr><th>Player</th><th>Role</th></tr>");
+		pw.println("<tr><td>Dhoni</td><td>Caption</td></tr>");
+		pw.println("<tr><td>Sachin</td><td>All Rounder</td></tr>");
+		pw.println("</table>");
+		//close PrintWriter
+		pw.close();
+	}//service
+
+}//class
